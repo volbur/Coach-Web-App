22db6d939a4cc16ef3a9934fa97a7c99e5547f54
Volodymyr Burkovskiy <volodymyr.b.pro@gmail.com>
2021-08-11 13:38:50 +0300

    Adding vuex and coach data

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -6,8 +6,24 @@
     <router-link custom v-slot="{ navigate }" :to="{ name: 'register' }">
       <button @click="navigate" class="register-btn">Register as Coach</button>
     </router-link>
-    <ul>
-      LIST OF COACHES
+    <ul v-if="hasCoaches">
+      <li v-for="coach in filteredCoaches" :key="coach.id">
+        {{ coach.firstName }}
+      </li>
     </ul>
+    <h3 v-else>No coaches found</h3>
   </section>
 </template>
+
+<script>
+export default {
+  computed: {
+    hasCoaches() {
+      return this.$store.getters['coaches/hasCoaches'];
+    },
+    filteredCoaches() {
+      return this.$store.getters['coaches/coaches'];
+    },
+  },
+};
+</script>
